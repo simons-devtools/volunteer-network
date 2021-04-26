f01eb5cbf3d9b8f5d2e3d125f905385a7c8d7ef3
“c-moX” <cmoxsh@gmail.com>
2021-04-26 17:44:04 +0600

    up to date all

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h3>This is home page...!</h3>
+        </div>
+    );
+};
+
+export default Home;
